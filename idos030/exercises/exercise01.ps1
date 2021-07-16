bdf010eb0892636fc10aac10ae66c995d11353f8
PageotD <damien.pageot@gmail.com>
2021-07-16 10:09:49 +0200

    Modif exercise01 IDOS030

--- idos030/exercises/exercise01.ps1
+++ idos030/exercises/exercise01.ps1
@@ -1,10 +1,20 @@
 # EXERCICE 01
+Function getSysEvents{
+    param(
+        # ID to search
+        [parameter(Mandatory = $true)]
+        [int]$searchId,
+        # Number of days for which the ID search must be done
+        [parameter(Mandatory = $true)]
+        [int]$ndays
+    )
 
-# On crée une variable Date qui contiendra la date à partir de laquelle on veut un retour de log
-# On écrit la valeur en sortie terminal pour être sûr
-$Date = (Get-Date).AddDays(-21)
-Write-Host "Date de référence: $Date"
+    # Get the starting date
+    $date = (Get-Date).Adddays(-1*$ndays)
 
-# On prend les évènements "Application" ayant l'ID 6005 et ayant lieu à partir de $Date
-# Le résultat est redirigé dans un fichier texte nommé "Start.txt"
-Get-WinEvent System | Where-Object {$_.id -eq 6005 -and $_.TimeCreated -ge $Date} > C:\Temp\Start.txt
+    # Search for events with ID=$searchId after $date and write output in c:\Temp\exercise01_start.txt
+    Get-WinEvent System | Where-Object {$_.id -eq 6005 -and $_.TimeCreated -ge $Date} > C:\Temp\exercise01_start.txt
+}
+
+# Call getSysEvents with parameters searchID (i.e. 6005) and the number of days (i.e. 21)
+getSysEvents -searchID 6005 -ndays 21
