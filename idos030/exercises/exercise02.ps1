0912bfede14f0aa1213c9e5d65784ad978bc0d65
PageotD <damien.pageot@gmail.com>
2021-07-16 11:33:03 +0200

    Modif exercise02 IDOS030

--- idos030/exercises/exercise02.ps1
+++ idos030/exercises/exercise02.ps1
@@ -1,31 +1,49 @@
 # EXERCICE 02
 
-# On teste l'existance du dossier C:\Temp\TestFinal
-# Si le dossier existe, on le fait apparaître dans le terminal
-# sinon, on le crée
-If(Test-Path -Path C:\Temp\TestFinal){
-    write-host "Le dossier TestFinal existe déjà"
+Function createFiles {
+    param(
+        # ID to search
+        [parameter(Mandatory = $true)]
+        [string]$fileName,
+        # Number of days for which the ID search must be done
+        [parameter(Mandatory = $true)]
+        [int]$nfile,
+        # Path to folder where the files will be stored
+        [parameter(Mandatory = $true)]
+        [string]$folderPath
+    )
+
+    # Check if folder exist
+    If(Test-Path -Path $folderPath){
+        write-host "Le dossier TestFinal existe déjà"
+    }
+    Else{
+        write-host "Le dossier TestFinal n'existe pas. Il va être créé."
+        New-Item -ItemType Directory -Path $folderPath
+    }
+
+    # On veut créer 76 fichiers dont le nom doit correspondre à la nomenclature "monfichierXXX.txt"
+    # On veut stocker le nom du fichier et sa date de création dans un tableau
+    # On commence par créer le tableau
+    [System.Collections.ArrayList]$tabFiles = @()
+
+    $date = (Get-Date)
+    For($i=0; $i -lt $nfile; $i++){
+        $item = New-Object -TypeName psobject -Property ([Ordered]@{
+            NomFichier = (-join($fileName, $i.ToString("000"), ".txt"))
+            DateCreation = $date})
+        write-host "DATE $date"
+        $tabFiles.add($item)
+        New-Item -ItemType File -force -Path (-join($folderPath, $fileName, $i.ToString("000"), ".txt"))
+    }
+
+    # On exporte le tableau au format XML
+    $tabFiles | Export-Clixml (-join($folderPath,"tabelau.xml"))
+
+    # On écrit le tableau formaté dans un fichier de sortie
+    $tabFiles | ft > (-join($folderPath,"tableau_sortie.txt"))
+
 }
-Else{
-    write-host "Le dossier TestFinal n'existe pas. Il va être créé."
-    New-Item -ItemType Directory -Path C:\Temp\TestFinal
-}
-# On veut créer 76 fichiers dont le nom doit correspondre à la nomenclature "monfichierXXX.txt"
-# On veut stocker le nom du fichier et sa date de création dans un tableau
 
-# On commence par créer le tableau
-[System.Collections.ArrayList]$tabFiles = @()
+createFiles -fileName "monfichier" -nfile 76 -folderPath "C:\Temp\TestFinalB\"
 
-For($i=0; $i -lt 76; $i++){
-    $item = New-Object -TypeName psobject -Property ([Ordered]@{
-    NomFichier = (-join("monfichier", $i.ToString("000"), ".txt"))
-    DateCreation = $Date})
-    $tabFiles.add($item)
-#    #New-Item -ItemType File -Path (-join("C:\Temp\TestFinal\monfichier", $i.ToString("000"), ".txt")) #$fileFull
-}
-
-# On exporte le tableau au format XML
-$tabFiles | Export-Clixml C:\Temp\TestFinal\tabelau.xml
-
-# On écrit le tableau formaté dans un fichier de sortie
-$tabFiles | ft > C:\Temp\TestFinal\tableau_sortie.txt
