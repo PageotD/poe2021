cb75b44727584fa64a6766c834137869facba243
PageotD <damien.pageot@gmail.com>
2021-07-19 12:50:31 +0200

    Update exercises IDOS

--- idos030/exercises/exercise01.ps1
+++ idos030/exercises/exercise01.ps1
@@ -9,11 +9,25 @@ Function getSysEvents{
         [int]$ndays
     )
 
-    # Get the starting date
-    $date = (Get-Date).Adddays(-1*$ndays)
+    # Get the starting time in ms format ndays*24*3600*1000
+    $startTime = $ndays*24*3600*1000
+    # Print the request date for confirmation
+    write-host (Get-Date).Adddays(-1*$ndays)
 
-    # Search for events with ID=$searchId after $date and write output in c:\Temp\exercise01_start.txt
-    Get-WinEvent System | Where-Object {$_.id -eq 6005 -and $_.TimeCreated -ge $Date} > C:\Temp\exercise01_start.txt
+    $customFilterXML = "
+        <QueryList>
+            <Query Id='0' Path='System'>
+                <Select Path='System'>*[System[(EventID=$searchID) and TimeCreated[timediff(@SystemTime) &lt;= $startTime]]] 
+                </Select>
+            </Query>
+        </QueryList>
+    "
+
+    # Search for events with ID=$searchId after $startDate and write output in c:\Temp\exercise01_start.txt
+    Get-WinEvent -FilterXml $customFilterXML | Out-File -FilePath C:\Temp\exercise01_start.txt
+    # Search for events with ID=$searchId after $startDate and write output in c:\Temp\exercise01_start.txt
+    #Get-WinEvent System | Where-Object {$_.id -eq 6005 -and $_.TimeCreated -ge $startDate} > C:\Temp\exercise01_start.txt
+    #Get-WinEvent System | Where-Object {$_.id -eq 6005 -and $_.TimeCreated -ge $startDate}  | Export-Clixml C:\Temp\export.xml
 }
 
 # Call getSysEvents with parameters searchID (i.e. 6005) and the number of days (i.e. 21)
--- idos030/exercises/exercise03.ps1
+++ idos030/exercises/exercise03.ps1
@@ -1,6 +1,37 @@
 # Exercice 03
 
+# On dÃ©marre la transcription
+#Start-Transcript
+
 # Dans un tableau interactif (GridView), on veut faire apparaitre les services en cours dâ€™exÃ©cution, en affichant les propriÃ©tÃ©s suivantes (headers) : 
 # NomService, ExeFullName, Description.
 #Get-WmiObject -Class Win32_Service | Format-List -Property *
-Get-WmiObject -Class "Win32_Service" | Select-Object -Property Name, PathName, Description | Out-GridView
+#Get-WmiObject -Class "Win32_Service" | Select-Object -Property Name, PathName, Description | Out-GridView
+$Global:winService
+
+# On dÃ©fini une fonction qui va stocker les infos "Win32_Service"
+Function getServices {
+    param(
+        # La classe dans laquelle ont fait la recherche de process
+        [parameter(Mandatory = $true)]
+        [string]$searchServices
+
+    )
+    # On veut rÃ©cupÃ©rer les services en cours d'execution "running"
+    # On initialise la variable de sortie
+    $selectedService = $null
+    Get-WmiObject -Class "Win32_Service" -filter "state = 'running'" | Out-GridView -OutputMode Multiple -Outvariable selectedService
+    # On boucle sur les services sélectionnés pour les stopper
+    if($selectedService.Count -eq 0){
+        write-host "Aucun service selectionné."
+    }
+    else{
+        $number_of_services = $selectedService.Count
+        write-host "Vous avez séléctioné $number_of_services service(s) pour arrêt."
+        Foreach($service in $selectedService){
+            Stop-Service -Name $service.Name -WhatIf
+        }
+    }
+}
+
+getServices -searchServices "Win32_Service"
